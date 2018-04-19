d5febd2d4d4614764a508428f68798a6dd27194b
skomaromi <skomaromi@etfos.hr>
2018-04-19 13:16:57 +0200

    Initial commit
    * added layouts for task items and the initial activity

---
+++ app/src/main/java/com/github/skomaromi/tasky/ui/TaskDisplayActivity.java
@@ -0,0 +1,15 @@
+package com.github.skomaromi.tasky.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.skomaromi.tasky.R;
+
+public class TaskDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_display);
+    }
+}
